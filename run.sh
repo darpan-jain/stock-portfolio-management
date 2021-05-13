24f8ae99eb3772a4d092a21c355a6cb5379af2f4
Darpan <darpan@adsparx.in>
2021-05-13 16:30:35 +0530

    Script for crontab updated

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/darpan/Documents/stock-tracker/
+cd /home/darpan/Documents/github/stock-portfolio-management
 source /home/darpan/Documents/virtual_envs/wormhole/bin/activate
-python3 main.py
+python3 main.py &
 echo "Update sent successfully"
