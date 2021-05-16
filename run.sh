0c88a05ec88b56dbb136fd426b614b068450a03f
Darpan <darpan@adsparx.in>
2021-05-16 19:12:38 +0530

    Script to run in background by cron job

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 cd /home/darpan/Documents/github/stock-portfolio-management
 source /home/darpan/Documents/virtual_envs/wormhole/bin/activate
 python3 main.py &
-echo "Update sent successfully"
+ps -ef | grep -i "main.py"
+pid=$(ps -ef | grep -i "main.py" | awk 'FNR == 1 {print $3}')
+echo "Update script running in the background with PID = $pid"
