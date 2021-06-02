7cfc2b456f3cba7bd6e8600ef1669948ff5c0df6
Darpan <darpan@adsparx.in>
2021-06-02 13:20:08 +0530

    Automated script updated

--- run.sh
+++ run.sh
@@ -5,3 +5,4 @@ python3 main.py &
 ps -ef | grep -i "main.py"
 pid=$(ps -ef | grep -i "main.py" | awk 'FNR == 1 {print $3}')
 echo "Update script running in the background with PID = $pid"
+tail -f logs/app.log
